8c4a047347492f48d754cfb02c0c69b39d94abc6
ZsoltJuhasz <juhasz.zsolt98@gmail.com>
2022-03-03 12:13:56 +0100

    Init

---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  employeeName = "Erős István";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  adTelepules(telepules: string){
+    console.log("Telepules",
+      telepules
+    )
+  }
+
+}
